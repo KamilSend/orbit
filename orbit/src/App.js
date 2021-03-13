19fddf006d0299ca374118cbd8a3fbe22f443959
kamilsendecki <sendej@gmail.com>
2021-03-13 10:21:49 +0100

    import bootstrap scss

--- orbit/src/App.js
+++ orbit/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
 
 function App() {
   return (
     <div className="App">
+        <Button>Button</Button>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
